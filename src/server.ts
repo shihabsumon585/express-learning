34a10ad40dea9b7f544b72b3915f4ac0d2be8575
Md Shihab Sumon <shihabsumon585@gmail.com>
2026-06-22 10:14:47 +0600

    connect with neon

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,35 @@
 // const express = require('express');
 import express, { type Application, type Request, type Response } from "express";
+import { Pool } from "pg";
 const app: Application = express()
 const port = 5000
 
-app.get('/', (req:Request, res: Response) => {
-  res.send('Hello World!')
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+
+const pool = new Pool({
+  connectionString: ""
+})
+
+app.get('/', (req: Request, res: Response) => {
+  // res.send('Hello World!')
+  res.status(200).json({
+    "message": "This is the root of the server!",
+    "author": "Next Level"
+  })
+})
+
+app.post("/", (req: Request, res: Response) => {
+  // console.log(req.body);
+  const { name, ID, password } = req.body;
+  res.status(201).json({
+    message: "Created...",
+    data: {
+      name,
+      ID
+    }
+  })
 })
 
 app.listen(port, () => {
