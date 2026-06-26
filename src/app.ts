dc9d3d2d7d7ed1eb8be84961b4caf99a5366b692
shihabsumon585 <shihabsumon585@gmail.com>
2026-06-26 17:53:16 +0600

    implement middleware and logger.txt

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,19 @@ import express, { type Application, type Request, type Response } from "express"
 import { userRoute } from "./modules/user/user.route";
 import { authRoute } from "./modules/auth/auth.route";
 import { profileRoute } from "./modules/profile/profile.route";
+import fs from "fs"
 const app: Application = express()
 
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log('Time:', Date.now());
+  const log = `\nMethod -> ${req.method}, URL -> ${req.url}, Date -> ${Date.now()}\n`;
+  fs.appendFile("logger.txt", log, (err) => console.log(err))
+  next();
+});
 
 app.get('/', (req: Request, res: Response) => {
   // res.send('Hello World!')
